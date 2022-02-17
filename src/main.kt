33f1e245cfa7f84a1abdd6ec0bfb5e3a8815de74
Happiness-James <gesarehjames@gmail.com>
2022-02-17 19:42:49 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    fullName()
+    phoneNumber()
+    weight()
+    citizen()
+
+}
+fun fullName(){
+    val name = "Happiness James"
+    println(name)
+}
+fun phoneNumber(){
+    val phoneNumber = "0720377261"
+    println(phoneNumber)
+}
+fun weight(){
+    val weight = 57.6
+    val weigh = "kg"
+    print(weight)
+    println(weigh)
+}
+fun citizen(){
+    val kenyan = true
+    println(kenyan)
+}
